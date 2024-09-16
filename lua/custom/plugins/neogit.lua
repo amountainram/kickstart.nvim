d5a4656790bc0e5e57a8f18fe5207be9e59c5137
Umberto Toniolo <amountainram@gmail.com>
2024-09-16 08:54:32 +0200

    feat: replace lazygit with neogit plugin

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed.
+    'nvim-telescope/telescope.nvim', -- optional
+    'ibhagwan/fzf-lua', -- optional
+    'echasnovski/mini.pick', -- optional
+  },
+  config = true,
+  keys = function(_, keys)
+    return {
+      { '<leader>ng', '<cmd>Neogit kind=tab<CR>', desc = 'Open Neogit' },
+      unpack(keys),
+    }
+  end,
+}
