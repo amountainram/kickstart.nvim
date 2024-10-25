e0cd6441671fe3e18aa8479dfd0e7ec12f6d7b4f
Umberto Toniolo <amountainram@gmail.com>
2024-10-25 21:05:41 +0200

    feat: add emoji on telescope

--- init.lua
+++ init.lua
@@ -380,6 +380,8 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      'hrsh7th/nvim-cmp',
+      'allaman/emoji.nvim',
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -424,6 +426,9 @@ require('lazy').setup({
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
 
+      pcall(require('emoji').setup, { enable_cmp_integration = true })
+      pcall(require('telescope').load_extension, 'emoji')
+
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -437,6 +442,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>se', function()
+        local ts = require('telescope').load_extension 'emoji'
+        ts.emoji()
+      end, { desc = '[S]earch [E]moji' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
